929973d2b3c347d908627f72ad7d17dfd7312a6c
Yusuke Hara <hara.inamake1902@gmail.com>
2018-07-13 11:28:33 +0900

    7/13 課題提出

--- api.rb
+++ api.rb
@@ -8,8 +8,8 @@ get '/show' do
   # Rubyのハッシュを作成、diaryという名前を付ける
   diary = {
       id: 1,
-      title: "今日の日記",
-      content: "きょうは雨でした。"
+      title: "7/6の天気",
+      content: "きょうも暑かった。"
   }
 
   # diaryをJSONテキストに変換して返す
