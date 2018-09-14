17b1b728609dfa64b7255b5fd1574cff1f94de0b
Hara Yusuke <38772322+inamake@users.noreply.github.com>
2018-09-14 10:48:37 +0900

    Add files via upload

---
+++ slackbot_settings.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# botアカウントのトークンを指定
+API_TOKEN = "xoxb-434201567056-434928687202-qBgVTAyuVseFs7CN6Y9FZGP8"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "わかりません"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
